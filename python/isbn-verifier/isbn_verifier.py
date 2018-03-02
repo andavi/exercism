2482f52bee0cf30d5ce8f13237b414bbe7db3dce
Andrew Davis <andrew.bradley.davis@gmail.com>
2018-03-01 21:40:02 -0500

    hamming

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -2,7 +2,9 @@ import re
 
 
 def verify(isbn):
-    # Create list of integers
+    # Create list of integers.
     nums = [10 if n == 'X' else int(n) for n in re.findall('[0-9X]', isbn)]
     
-    return len(nums) == 10 and 10 not in nums[:9] and sum([a*b for a,b in zip(nums, range(10, 0, -1))]) % 11 == 0
+    # Check length, check digit, and modulo.
+    return len(nums) == 10 and 10 not in nums[:9] \
+        and sum([a*b for a,b in zip(nums, range(10, 0, -1))]) % 11 == 0
